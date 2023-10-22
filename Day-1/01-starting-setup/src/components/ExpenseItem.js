6abd74bcb9be11b3751ff47a195923ad30b281c0
Utkarsh Mishra <nasautkarsh@gmail.com>
2023-10-22 23:21:34 +0530

    Day-2
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- Day-1/01-starting-setup/src/components/ExpenseItem.js
+++ Day-1/01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
-function ExpenseItem(){
-    return(
-        <div>
-            <div>Date</div>
-            <div>
-                <h2>Title</h2>
-                <div>Amount</div>
-            </div>
-        </div>
-    );
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
