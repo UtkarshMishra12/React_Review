256409d4eca598ba5592fb2d839c8a28d0d40011
Utkarsh Mishra <nasautkarsh@gmail.com>
2023-11-03 01:24:06 +0530

    Basic Logs Functionality
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- Tic-Tac-Toe_Project/src/App.jsx
+++ Tic-Tac-Toe_Project/src/App.jsx
@@ -1,17 +1,36 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+import Log from "./components/Log";
 
 const App = () => {
+  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns]= useState([]);
+
+  function handleSelectSquare(rowIndex, colIndex){
+    setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns=> {
+      let currentPlayer= "X";
+
+      if(prevTurns.length > 0 &&  prevTurns[0].player=== 'X'){
+        currentPlayer= 'O';
+      }
+      const updatedTurns = [ {square : {row:rowIndex, col:colIndex} , player: currentPlayer}, ...prevTurns];
+
+      return updatedTurns;
+    });
+  }
   return (
     <>
       <main>
         <div id="game-container">
-          <ol id="players">
-            <Player initialName="Player 1" symbol="x" />
-            <Player initialName="Player 2" symbol="O" />
+          <ol id="players" className="highlight-player">
+            <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+            <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
           </ol>
-          GAME
+          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
         </div>
-        LOGS
+        <Log turns={gameTurns}/>
       </main>
     </>
   );
