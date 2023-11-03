b17359c8dbef2621a999fc7173508f1dd7efdd98
Utkarsh Mishra <nasautkarsh@gmail.com>
2023-11-03 23:55:43 +0530

    Tic-Tac-Toe Project Done
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- Tic-Tac-Toe_Project/src/components/GameBoard.jsx
+++ Tic-Tac-Toe_Project/src/components/GameBoard.jsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-const GameBoard = ({onSelectSquare, turns}) => {
-    let gameBoard= initialGameBoard;
-
-    for(const turn of turns){
-        const {square, player}= turn;
-        const {row, col}= square;
-
-        gameBoard[row][col]= player;
-    }
+const GameBoard = ({onSelectSquare, board}) => {
 //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
 //   function handleSelectSquare(rowIndex, colIndex) {
@@ -30,12 +17,12 @@ const GameBoard = ({onSelectSquare, turns}) => {
 //   }
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((palyerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={palyerSymbol!=null}>
                   {palyerSymbol}
                 </button>
               </li>
